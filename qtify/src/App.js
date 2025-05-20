acac6394fd4e278d0117ba24d56822d1b42849e7
Mahima <priyanshisrivastava708016@gmail.com>
2025-05-21 02:15:42 +0530

    hello React

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -9,8 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-        >
+        <a>
           Learn React
         </a>
         <p>Hello to Vercel"</p>
