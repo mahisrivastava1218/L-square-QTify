3ac120b6639ad1705b30602e3f118153461d4ecd
Mahima <priyanshisrivastava708016@gmail.com>
2025-05-22 15:35:26 +0530

    npm run build

--- qtify/src/Home.js
+++ qtify/src/Home.js
@@ -1,4 +1,3 @@
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero'
 function Home() {
