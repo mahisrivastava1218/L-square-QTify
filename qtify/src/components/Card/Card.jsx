76d74402ce903146255afd790e05d08da7da04a0
Mahima <priyanshisrivastava708016@gmail.com>
2025-05-24 03:00:40 +0530

    swiper

--- qtify/src/components/Card/Card.jsx
+++ qtify/src/components/Card/Card.jsx
@@ -3,6 +3,13 @@ import AlbumCard from "./ProductCard";
 import axios from "axios";
 import { Grid } from "@mui/material";
 import styles from "./Card.module.css";
+import {Swiper, SwiperSlide} from "swiper/react";
+import {Navigation} from "swiper/modules"
+import 'swiper/css';
+import 'swiper/css/navigation';
+import leftArrow from "../../assets/LeftArrow.svg";
+import rightArrow from "../../assets/RightArrow.svg";
+
 // react component named start with capital letter
 //make react component call api store in setProdutcs,setFilteredProducts using hook useState call api using useEffect hook empty dependency arr
 export default function MyCard(){
@@ -32,7 +39,7 @@ export default function MyCard(){
 useEffect(()=>{
     fetchTopAlbums();
     fetchNewAlbums();
-},[])
+},[]);
 
 const Section =({productProp,albums})=>{
     console.log(albums.length);
@@ -43,12 +50,39 @@ const Section =({productProp,albums})=>{
          <span className={styles.collapse}>Show all</span>
     </div>
     <Grid container className={styles.myCardGrid}>
-    {albums.length>0 ? (
+    {/* {albums.length>0 ? (
       albums.map((product,index)=>(
         <Grid item key={index}>
          <AlbumCard  productProp={product}/>
          </Grid>
-      ))
+      )) */}
+       {albums.length>0 ? (
+        <>
+        <div className={styles.customprev}><img src={leftArrow} alt="Left Arrow"/></div>
+      <Swiper 
+      modules={[Navigation]}
+      navigation={{
+        nextEl: `${styles.customnext}`,
+        prevEl: `${styles.customprev}`
+      }}
+      // spaceBetween={32}
+      slidesPerView={7}
+      scrollbar={{ draggable: true }}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+      >
+        {albums.map((album,index)=>{
+          return(
+            <>
+          <SwiperSlide key={index}>
+          <AlbumCard productProp={album}/>
+          </SwiperSlide>
+            </>
+          )
+        })}
+      </Swiper> 
+     <div className={styles.customnext}><img src={rightArrow} alt="right Arrow"/></div>
+</>
     ):(
         <p>No album available</p>
     )}
